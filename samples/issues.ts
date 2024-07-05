419a5759e3d5d625cbb8586dd654a4b1844adead
srivastavarahull <139200493+srivastavarahull@users.noreply.github.com>
2024-07-05 16:25:54 +0530

    fixed imports in sample

--- samples/issues.ts
+++ samples/issues.ts
@@ -1,11 +1,16 @@
-import { IssueClient } from "./custom-code/IssueClient";
+//process.env['LOG_LEVEL'] = 'info'; /* The default log level is `Error`.*/
+import { CommentsPayload, IssueClient ,IssuePayload, Region, Status  } from "@aps_sdk/construction-issues";
 import { SdkManager, SdkManagerBuilder } from "@aps_sdk/autodesk-sdkmanager";
-import { IssuePayload, Region, Status } from "./model";
-import { isDataView } from "util/types";
 
 const access_token = "<token>";
 const projectid: string = "<projectid>";
-
+const issueid:string="<issueid>";
+const issuepayload: IssuePayload = {
+    title: "<title>",
+    description: "<description>",
+    status: Status.Open,
+    issueSubtypeId: "<issueSubtypeId>"
+}
 
 var sdkManager: SdkManager = SdkManagerBuilder
     .create()
@@ -15,42 +20,42 @@ const issuesClient: IssueClient = new IssueClient(sdkManager);
 //Returns the current user permissions.
 async function getUserProfile()
 {
-    var response = await issuesClient.getUserProfile(token, projectid)
+    var response = await issuesClient.getUserProfile(access_token, projectid)
     console.log(response);
 }
 
 //Retrieves a project’s categories and types. 
 async function getIssuesTypes()
 {
-    var response = await await issuesClient.getIssuesTypes(token,projectid);
+    var response = await await issuesClient.getIssuesTypes(access_token,projectid);
     console.log(response);
 }
 
 //Retrieves information about issue custom attributes (custom fields) for a project, including the custom attribute title, description and type.
 async function getAttributeDefinitions()
 {
-    var response = await issuesClient.getAttributeDefinitions(token, projectid, { xAdsRegion: Region.Us });
+    var response = await issuesClient.getAttributeDefinitions(access_token, projectid, { xAdsRegion: Region.Us });
     console.log(response);
 }
 
 //Retrieves information about the issue custom attributes (custom fields) that are assigned to issue categories and issue types
 async function getAttributeMappings()
 {
-    var response = issuesClient.getAttributeMappings(token,projectid);
+    var response = issuesClient.getAttributeMappings(access_token,projectid);
     console.log(response);
 }
 
 //Retrieves a list of supported root cause categories and root causes that you can allocate to an issue. For example, communication and coordination.
 async function getRootCauseCategories()
 {
-    var response = await issuesClient.getRootCauseCategories(token, projectid, { filterUpdatedAt: "<DATETIME>,<DATETIME>"});
+    var response = await issuesClient.getRootCauseCategories(access_token, projectid, { filterUpdatedAt: "<DATETIME>,<DATETIME>"});
     console.log(response);
 }
 
 // Retrieves information about all the issues in a project, including details about their associated comments and attachments.
 async function getIssues()
 {
-    var response = await issuesClient.getIssues(token, projectid);
+    var response = await issuesClient.getIssues(access_token, projectid);
     console.log(response);
 }
 
@@ -58,34 +63,29 @@ async function getIssues()
 async function createIssue()
 {
 
-    const issuepayload: IssuePayload = {
-        title: "<title>",
-        description: "<description>",
-        status: Status.Open,
-        issueSubtypeId: "<issueSubtypeId>"
-    }
-    var response = await issuesClient.createIssue(token, projectid, issuepayload, { xAdsRegion: Region.Us });
+    
+    var response = await issuesClient.createIssue(access_token, projectid, issuepayload, { xAdsRegion: Region.Us });
     console.log(response);
 }
 
 //Retrieves detailed information about a single issue. For general information about all the issues in a project, see GET issues.
 async function getIssueDetails()
 {
-    var response = await issuesClient.getIssueDetails(token,projectid,issueid);
+    var response = await issuesClient.getIssueDetails(access_token,projectid,issueid);
     console.log(response);
 }
 
 //Updates an issue.
 async function patchIssueDetails()
 {
-    var response = await issuesClient.patchIssueDetails(token,projectid,issueid,issuepayload);
+    var response = await issuesClient.patchIssueDetails(access_token,projectid,issueid,issuepayload);
     console.log(response);
 }
 
 // Get all the comments for a specific issue.
 async function getComments()
 {
-    var response = await issuesClient.getComments(token,projectid,issueid);
+    var response = await issuesClient.getComments(access_token,projectid,issueid);
     console.log(response);
 }
 
@@ -94,9 +94,8 @@ async function createComments()
 {
     var commentsPayload :CommentsPayload={
             body:"<Body of the comment>"
-
         }
-    var response = await  issuesClient.createComments(token,projectid,issueid,commentsPayload);
+    var response = await  issuesClient.createComments(access_token,projectid,issueid,commentsPayload);
     console.log(response);
 }
 
