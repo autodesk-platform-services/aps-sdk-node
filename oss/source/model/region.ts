feb78f9f1ebd67841d9a7dfb541d5346f027448e
srivastavarahull <139200493+srivastavarahull@users.noreply.github.com>
2025-01-21 14:03:32 +0530

    Added Support for AUS Region

--- oss/source/model/region.ts
+++ oss/source/model/region.ts
@@ -1,9 +1,11 @@
 /* tslint:disable */
 /* eslint-disable */
 
+import { deprecate } from "util";
+
 
 /**
- * Specifies where the bucket containing the object is stored. Possible values are:          - ``US`` - (Default) Data center for the US region.         - ``EMEA`` - Data center for the European Union, Middle East, and Africa.         - ``APAC`` -  (Beta) Data center for Australia.          **Note:** Beta features are subject to change. Please do not use in production environments.
+ * Specifies where the bucket containing the object is stored. Possible values are:          - ``US`` - (Default) Data center for the US region.         - ``EMEA`` - Data center for the European Union, Middle East, and Africa.         - ``AUS`` -  (Beta) Data center for Australia.          **Note:** Beta features are subject to change. Please do not use in production environments.
  * @export
  * @enum {string}
  */
@@ -11,7 +13,11 @@
 export const Region = {
     Us: 'US',
     Emea: 'EMEA',
-    Apac: 'APAC'
+    /**
+     * @deprecated This enum value is deprecated and will be removed in future versions.
+     */
+    Apac: 'APAC',
+    AUS: 'AUS'
 } as const;
 
 export type Region = typeof Region[keyof typeof Region];
