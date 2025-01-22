d3b6346405ef84659cb2f18ca0b538d05e27afdd
Harun Gitundu <harun.gitundu@autodesk.com>
2025-01-22 05:31:49 +0300

    Add AUS region support for datamanagement

--- datamanagement/source/model/region.ts
+++ datamanagement/source/model/region.ts
@@ -1,9 +1,12 @@
 /* tslint:disable */
 /* eslint-disable */
 
-
 /**
- * Specifies where the hub is stored. Possible values are:  - ``US`` - Data center for the US region. - ``EMEA`` - Data center for the European Union, Middle East, and Africa regions. - ``APAC`` - Data center for the Australia region.
+ * Specifies where the hub is stored. Possible values are:  
+ * - ``US`` - Data center for the US region.  
+ * - ``EMEA`` - Data center for the European Union, Middle East, and Africa regions.  
+ * - ``APAC`` - Data center for the Australia region. **@deprecated Use `AUS` instead.**  
+ * - ``AUS`` - New data center for the Australia region.  
  * @export
  * @enum {string}
  */
@@ -11,10 +14,9 @@
 export const Region = {
     Us: 'US',
     Emea: 'EMEA',
-    Apac: 'APAC'
+    /** @deprecated Use `AUS` instead. */
+    Apac: 'APAC',
+    Aus: 'AUS'
 } as const;
 
 export type Region = typeof Region[keyof typeof Region];
-
-
-
