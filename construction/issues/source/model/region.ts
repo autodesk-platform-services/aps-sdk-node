f80fde6b4e8e301ee8a5cf357d5a809dfb723bf6
srivastavarahull <139200493+srivastavarahull@users.noreply.github.com>
2025-01-23 11:35:21 +0530

    Added AUS region support for Issues

--- construction/issues/source/model/region.ts
+++ construction/issues/source/model/region.ts
@@ -3,7 +3,7 @@
 
 
 /**
- * Specifies where the bucket containing the object is stored. Possible values are:             - ``US`` - (Default) Data center for the US region.             - ``EMEA`` - Data center for the European Union, Middle East, and Africa.             - ``APAC`` -  (Beta) Data center for Australia. **Note:** Beta features are subject to change. Please do not use in production environments.
+ * Specifies where the bucket containing the object is stored. Possible values are:             - ``US`` - (Default) Data center for the US region.             - ``EMEA`` - Data center for the European Union, Middle East, and Africa.             - ``AUS`` -  Data center for Australia. 
  * @export
  * @enum {string}
  */
@@ -11,7 +11,11 @@
 export const Region = {
     Us: 'US',
     Emea: 'EMEA',
-    Apac: 'APAC'
+    /**
+    * @deprecated This enum value is deprecated and will be removed in future versions.
+    */
+   Apac: 'APAC',
+   AUS: 'AUS'
 } as const;
 
 export type Region = typeof Region[keyof typeof Region];
