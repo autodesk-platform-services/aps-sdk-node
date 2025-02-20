25604f6adaed30deff57c490a595204591d5ed46
Nishchhal Dagar <nishchhal@autodesk.com>
2025-02-20 17:35:48 +0530

    added comments in webhooks client and statusfilter bug fix

--- webhooks/source/model/statusFilter.ts
+++ webhooks/source/model/statusFilter.ts
@@ -3,7 +3,10 @@
 
 
 /**
- * Filters retrieved webhooks by their current state. Possible values are   - ``active`` - Successfully delivered most recent event notifications.  - ``inactive`` - Failed to deliver most recent event notification and has been deactivated. - ``reactivated`` - Previously inactive but was reactivated. No events have occurred since reactivation.  If this parameter is not specified, the filter is not applied. See [Event Delivery Guarantees](/en/docs/webhooks/v1/developers_guide/event-delivery-guarantees/) for more information on how the state of a webhook changes. 
+ * Filters retrieved webhooks by their current state. Possible values are   
+ * - ``active`` - Successfully delivered most recent event notifications.  
+ * - ``inactive`` - Failed to deliver most recent event notification and has been deactivated. 
+ * - ``reactivated`` - Previously inactive but was reactivated. No events have occurred since reactivation.  If this parameter is not specified, the filter is not applied. See [Event Delivery Guarantees](/en/docs/webhooks/v1/developers_guide/event-delivery-guarantees/) for more information on how the state of a webhook changes. **@deprecated Valid options are `active` | `inactive`** 
  * @export
  * @enum {string}
  */
@@ -11,6 +14,7 @@
 export const StatusFilter = {
     Active: 'active',
     Inactive: 'inactive',
+    /** @deprecated Valid options are `active` | `inactive`. */
     Reactivated: 'reactivated'
 } as const;
 
