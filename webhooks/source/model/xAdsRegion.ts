7f8bdd5154d8b9c9328f9b385e53fc57e92cd905
Rahul Srivastava <rahul.srivastava@autodesk.com>
2025-06-26 17:06:34 +0530

    Added support for Uk+4 for region enum

--- webhooks/source/model/xAdsRegion.ts
+++ webhooks/source/model/xAdsRegion.ts
@@ -3,11 +3,19 @@
 
 
 /**
- * Specifies the geographical location (region) of the server that the request is executed on. Supported values are the following, but the default value is US:
+ * Specifies the geographical location (region) of the server a request must be executed on. This also corresponds to the region where the Webhook data is stored. It is also the location of the server that will make requests to your callback URL. Possible values:
+ * 
  * - ``US`` - (Default) Data center dedicated to serve the United States region.
  * - ``EMEA`` - Data center dedicated to serve the European Union, Middle East, and Africa regions.
- * - ``APAC`` - (Beta) Data center dedicated to serve the Australia region.  **Note:** Beta features are subject to change. Please avoid using them in production environments.**@deprecated Use `AUS` instead.**
- * - ``AUS`` - (Beta) Register new webhooks in a data center dedicated to serve Australia.
+ * - ``AUS`` - (Beta) Data center dedicated to serve the Australia region.
+ * - ``CAN`` - Data center dedicated to serve the Canada region.
+ * - ``DEU`` - Data center dedicated to serve the Germany region.
+ * - ``IND`` - Data center dedicated to serve the India region.
+ * - ``JPN`` - Data center dedicated to serve the Japan region.
+ * - ``GBR`` - Data center dedicated to serve the United Kingdom region.
+ * 
+ * **Note:** Beta features are subject to change. Please avoid using them in production environments.
+ * 
  * @export
  * @enum {string}
  */
@@ -17,7 +25,12 @@ export const XAdsRegion = {
     Emea: 'EMEA',
     /** @deprecated Use `AUS` instead. */
     Apac: 'APAC',
-    Aus: 'AUS'
+    Aus: 'AUS',
+    Can: 'CAN',
+    Deu: 'DEU',
+    Ind: 'IND',
+    Jpn: 'JPN',
+    Gbr: 'GBR'
 } as const;
 
 export type XAdsRegion = typeof XAdsRegion[keyof typeof XAdsRegion];
